5b258585eeeb9b2d2321c1ab7093849f34a8c7b5
w1109 <a18277278902@gmail.com>
2019-02-03 18:26:49 +0800

    end

---
+++ src/main/java/com/imooc/service/ProductService.java
@@ -0,0 +1,25 @@
+package com.imooc.service;
+
+import com.imooc.dataobject.ProductInfo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+/**
+ *商品
+ * Created by 韦师兄
+ * 2019-02-03 17:33
+ */
+public interface ProductService {
+    ProductInfo findOne(String productId);
+//    查询所有在架商品列表
+    List<ProductInfo> findUpAll();
+
+    Page<ProductInfo> findAll(Pageable pageable);
+
+    ProductInfo save(ProductInfo productInfo);
+//    加库存
+//    减库存
+}
+
