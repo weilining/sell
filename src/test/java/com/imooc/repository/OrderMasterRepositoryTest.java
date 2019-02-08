99babeb44ecc0535c5857d192188c1bcd2c088e8
w1109 <a18277278902@gmail.com>
2019-02-09 01:28:07 +0800

    end

--- src/test/java/com/imooc/repository/OrderMasterRepositoryTest.java
+++ src/test/java/com/imooc/repository/OrderMasterRepositoryTest.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.math.BigDecimal;
@@ -22,15 +24,17 @@ public class OrderMasterRepositoryTest {
 
     @Autowired
     private OrderMasterRepository repository;
+
+    private final String OPENID = "110110";
     @Test
     public void saveTest(){
         OrderMaster orderMaster=new OrderMaster();
-        orderMaster.setOrderId("123456");
+        orderMaster.setOrderId("123457");
         orderMaster.setBuyerName("师兄");
         orderMaster.setBuyerPhone("123456789123");
         orderMaster.setBuyerAddress("慕课网");
         orderMaster.setBuyerOpenid("110110");
-        orderMaster.setOrderAmount(new BigDecimal(2.3));
+        orderMaster.setOrderAmount(new BigDecimal(2.5));
         OrderMaster result = repository.save(orderMaster);
         Assert.assertNotNull(result);
     }
@@ -38,5 +42,9 @@ public class OrderMasterRepositoryTest {
 
     public void findByBuyerOpenid() throws Exception{
 
+        PageRequest request=new PageRequest(1,3);
+        Page<OrderMaster> result = repository.findByBuyerOpenid(OPENID, request);
+        Assert.assertNotEquals(0,result.getTotalElements());
+//        System.out.println(result.getTotalElements());
     }
 }
