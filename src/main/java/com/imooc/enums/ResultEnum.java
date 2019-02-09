34a43ba5efd1b903189b056d9c78d0469d1201ce
w1109 <a18277278902@gmail.com>
2019-02-10 01:42:27 +0800

    end

--- src/main/java/com/imooc/enums/ResultEnum.java
+++ src/main/java/com/imooc/enums/ResultEnum.java
@@ -21,6 +21,8 @@ public enum ResultEnum {
     ORDER_UPDATE_FAIL(15,"订单更新失败"),
 
     ORDER_DETAIL_EMPTY(16,"订单详情为空"),
+
+    ORDER_PAY_STATUS_ERROR(17,"订单支付状态不正确")
     ;
     private Integer code;
     private String message;
--- src/test/java/com/imooc/service/impl/OrderServiceImplTest.java
+++ src/test/java/com/imooc/service/impl/OrderServiceImplTest.java
@@ -3,6 +3,7 @@ package com.imooc.service.impl;
 import com.imooc.dataobject.OrderDetail;
 import com.imooc.dto.OrderDTO;
 import com.imooc.enums.OrderStatusEnum;
+import com.imooc.enums.PayStatusEnum;
 import com.imooc.service.OrderService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
@@ -87,9 +88,16 @@ public class OrderServiceImplTest {
 
     @Test
     public void finish() {
+        OrderDTO orderDTO = orderService.findOne(ORDER_ID);
+        OrderDTO result = orderService.finish(orderDTO);
+        Assert.assertEquals(OrderStatusEnum.FINISHED.getCode(), result.getOrderStatus());
     }
 
     @Test
     public void paid() {
+        OrderDTO orderDTO = orderService.findOne(ORDER_ID);
+        OrderDTO result = orderService.paid(orderDTO);
+        Assert.assertEquals(PayStatusEnum.SUCCESS.getCode(), result.getPayStatus());
+
     }
 }
