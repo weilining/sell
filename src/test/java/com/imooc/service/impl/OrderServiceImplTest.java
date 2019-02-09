6300a2a094b176c6f9f7ce1b207821af2e26d9c5
w1109 <a18277278902@gmail.com>
2019-02-10 00:33:01 +0800

    end

--- src/test/java/com/imooc/service/impl/OrderServiceImplTest.java
+++ src/test/java/com/imooc/service/impl/OrderServiceImplTest.java
@@ -9,6 +9,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
@@ -28,8 +30,11 @@ public class OrderServiceImplTest {
     @Autowired
     private OrderServiceImpl orderService;
     private final String BUYER_OPENID="110110";
+
+    private final String ORDER_ID="1549698307977235124";
     @Test
     public void create() {
+
         OrderDTO orderDTO = new OrderDTO();
         orderDTO.setBuyerName("韦师兄");
         orderDTO.setBuyerAddress("慕课网");
@@ -38,13 +43,13 @@ public class OrderServiceImplTest {
 
 
         //购物车
-        List<OrderDetail> orderDetailList=new ArrayList<>();
+        List<OrderDetail> orderDetailList = new ArrayList<>();
         OrderDetail o1 = new OrderDetail();
         o1.setProductId("1234568");
         o1.setProductQuantity(1);
 
 
-        OrderDetail o2=new OrderDetail();
+        OrderDetail o2 = new OrderDetail();
         o2.setProductId("123457");
         o2.setProductQuantity(2);
 
@@ -54,16 +59,22 @@ public class OrderServiceImplTest {
         orderDTO.setOrderDetailList(orderDetailList);
 
         OrderDTO result = orderService.create(orderDTO);
-        log.info("【创建订单】 result={}",result);
+        log.info("【创建订单】 result={}", result);
         Assert.assertNotNull(result);
     }
 
     @Test
     public void findOne() {
+        OrderDTO result = orderService.findOne(ORDER_ID);
+        log.info("【查询单个订单】result={}",result);
+        Assert.assertEquals(ORDER_ID, result.getOrderId());
     }
 
     @Test
     public void findList() {
+        PageRequest request = new PageRequest(0, 2);
+        Page<OrderDTO> orderDTOPage = orderService.findList(BUYER_OPENID, request);
+        Assert.assertNotEquals(0,orderDTOPage.getTotalElements());
     }
 
     @Test
