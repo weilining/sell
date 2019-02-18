c33d4f88871de3501d129bec38a59db403d90c87
w1109 <a18277278902@gmail.com>
2019-02-18 11:09:46 +0800

    end

--- src/main/java/com/imooc/controller/WeixinController.java
+++ src/main/java/com/imooc/controller/WeixinController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by 韦师兄
@@ -19,6 +20,10 @@ public class WeixinController {
     public void auth(@RequestParam("code")String code){
         log.info("进入auto方法。。。");
         log.info("code={}", code);
+        String url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=wxc8951dba20e8ba76&secret=caff618149de3e744ac6beab42047bc1&code=" + code + "&grant_type=authorization_code";
+        RestTemplate restTemplate = new RestTemplate();
+        String response = restTemplate.getForObject(url, String.class);
+        log.info("response={}", response);
     }
 
 }
