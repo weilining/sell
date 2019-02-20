cfb1a69bda44ce2cddd0c590a471ce55898950a5
abstract <18277278902@163.com>
2019-02-20 13:16:06 +0800

    test

--- src/main/java/com/imooc/controller/SellerOrderController.java
+++ src/main/java/com/imooc/controller/SellerOrderController.java
@@ -1,8 +1,13 @@
 package com.imooc.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by 韦师兄
  * 2019-02-20 13:05
  */
+@Controller
+@RequestMapping("/seller/order")
 public class SellerOrderController {
 }
