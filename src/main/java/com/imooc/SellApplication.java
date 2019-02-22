e20aad1a93cd937975b6b390a80e1a3bde6a7fdc
weilining <18277278902@163.com>
2019-02-23 01:35:11 +0800

    end

--- src/main/java/com/imooc/SellApplication.java
+++ src/main/java/com/imooc/SellApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SellApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SellApplication.class, args);
     }
-
 }
 
