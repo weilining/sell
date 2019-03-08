99ac8a06bc0804ac46265641328721a4731039f2
weilining <18277278902@163.com>
2019-03-08 13:05:38 +0800

    12-8

---
+++ src/main/java/com/imooc/exception/SellerAuthorizeException.java
@@ -0,0 +1,9 @@
+package com.imooc.exception;
+
+/**
+ * Created by 韦师兄
+ * 2019-03-08 12:51
+ */
+public class SellerAuthorizeException extends RuntimeException{
+
+}
---
+++ src/main/java/com/imooc/handler/SellExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.imooc.handler;
+
+import com.imooc.config.ProjectUrlConfig;
+import com.imooc.exception.SellerAuthorizeException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by 韦师兄
+ * 2019-03-08 12:55
+ */
+@ControllerAdvice
+public class SellExceptionHandler {
+
+    @Autowired
+    private ProjectUrlConfig projectUrlConfig;
+
+    //拦截登录异常
+    @ExceptionHandler(value = SellerAuthorizeException.class)
+    public ModelAndView handlerAuthorizeException(){
+        return new ModelAndView("redirect:"
+        .concat(projectUrlConfig.getWechatOpenAuthorize())
+        .concat("/sell/wechat/qrAuthorize")
+        .concat("?returnUrl=")
+        .concat(projectUrlConfig.getSell())
+        .concat("/sell/seller/login"));
+
+    }
+}
